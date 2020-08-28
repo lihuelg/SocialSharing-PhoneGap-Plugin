3adf0f557e854a58f49fc2216e8f17cfd979a809
Lautaro Galarza <lautarogalarza@gmail.com>
2020-08-27 21:35:34 -0300

    Share SMS image using Base64

--- src/ios/SocialSharing.m
+++ src/ios/SocialSharing.m
@@ -521,15 +521,8 @@ static NSString *const kShareOptionIPadCoordinates = @"iPadCoordinates";
     if (image != nil && image != (id)[NSNull null]) {
       BOOL canSendAttachments = [[MFMessageComposeViewController class] respondsToSelector:@selector(canSendAttachments)];
       if (canSendAttachments) {
-        NSData *imageData = [NSData dataWithContentsOfURL:[NSURL URLWithString:image]];          
-        if (imageData != nil) {
-            [picker addAttachmentData:imageData typeIdentifier:@"public.data" filename:@"Image.png"];
-        } else {
-            NSURL *file = [self getFile:image];
-          if (file != nil) {
-            [picker addAttachmentURL:file withAlternateFilename:nil];              
-          }        
-        }
+        NSData *imageData = [[NSData alloc] initWithBase64EncodedString:image options:0];
+        [picker addAttachmentData:imageData typeIdentifier:@"public.data" filename:@"Image.png"];
       }
     }
 
